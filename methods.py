6a144c5131c6763b0b5255c5a5098dbb4a45412d
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-30 10:11:36 +0300

    Learn @Wendy-tugz type casting

---
+++ methods.py
@@ -0,0 +1,26 @@
+name = "Bro Code"
+
+# print(len(name))
+# print(name.find("C"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("o"))
+# print(name.replace("o", "a"))
+# print(name*4)
+
+# type casting = converting a data type of a value to another data type
+
+x = 1
+y = 2.0
+z = "3"
+
+# x = float(x)
+# y = float(y)
+# z = float(z)
+
+print(x)
+print(y)
+print(z*3)
