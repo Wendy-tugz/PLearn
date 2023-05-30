1ccf12f34bed3184d08f65a6a98a7945adfb9af2
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-30 09:54:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# first_name = "Bro"
+# last_name = "Code"
+# full_name = first_name +" "+ last_name
+#
+# print("Hello " + full_name)
+# print(type(full_name))
+
+# age = "21"
+# age += 1
+# print(age)
+# print(type(age))
+
+# multiple assignment
+# name = "Wendy"
+# age = 20
+# attractive = True
+
+# name, age, attractive = "Wendy", 20, True
+#
+# print(name)
+# print(age)
+# print(attractive)
+
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
