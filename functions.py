a53be813244aec2680cbf38219bbdfe48040a5f0
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-31 12:35:32 +0300

    Learn @Wendy-tugz functions(return statement)

--- functions.py
+++ functions.py
@@ -1,8 +1,18 @@
 # functions = a block of code which is executed only when it is called.
 
-def hello(first_name, last_name, age):
-    print("hello! " + first_name + " " + last_name)
-    print("You are " + str(age) + " " + "years old")
-    print("Have a nice day!")
+# def hello(first_name, last_name, age):
+#     print("hello! " + first_name + " " + last_name)
+#     print("You are " + str(age) + " " + "years old")
+#     print("Have a nice day!")
+#
+# hello("Wendy", "Code", 21)
 
-hello("Wendy", "Code", 21)
+
+
+# return statement
+
+def multiply(number1, number2):
+    return number1 * number2
+
+x = multiply(6, 8)
+print(x)
