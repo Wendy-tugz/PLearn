50df23aa0822c176885d23698320ef002096468b
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-31 11:57:02 +0300

    Learn @Wendy-tugz functions

---
+++ functions.py
@@ -0,0 +1,7 @@
+# functions = a block of code which is executed only when it is called.
+
+def hello(first_name, last_name):
+    print("hello! " + first_name + " " + last_name)
+    print("Have a nice day!")
+
+hello("Wendy", "Code")
