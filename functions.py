6539777a7477de0436d7bf3a88a3778df5a51fd9
Wendy-tugz <wtugahirwa@gmail.com>
2023-05-31 12:07:57 +0300

    Learn @Wendy-tugz functions

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
 # functions = a block of code which is executed only when it is called.
 
-def hello(first_name, last_name):
+def hello(first_name, last_name, age):
     print("hello! " + first_name + " " + last_name)
+    print("You are " + str(age) + " " + "years old")
     print("Have a nice day!")
 
-hello("Wendy", "Code")
+hello("Wendy", "Code", 21)
