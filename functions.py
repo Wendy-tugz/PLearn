cf8009165f0671f2f8ac77b51c18f6bf6e4a20a2
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-02 15:56:28 +0300

    Learn @Wendy-tugz exception handling

--- functions.py
+++ functions.py
@@ -122,7 +122,6 @@ try:
     numerator = int(input("Enter a number to divide: "))
     denominator = int(input("Enter a number to divide by: "))
     result = numerator / denominator
-    print(result)
 except ZeroDivisionError as e:
     print(e)
     print("You can't divide by zero! idiot!")
@@ -132,6 +131,9 @@ except ValueError as e:
 except Exception as e:
     print(e)
     print("Something went wrong.")
-
+else:
+    print(result)
+finally:
+    print("This will always execute.")
 
 
