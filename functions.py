111ac25bc867a8c4910e11a09f24521c8a769db9
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-02 11:50:35 +0300

    Learn @Wendy-tugz str.format

--- functions.py
+++ functions.py
@@ -71,9 +71,11 @@
 
 # str.format() method = optional method that gives users more control when displaying output
 
-animal = "cow"
-item = "moon"
+# animal = "cow"
+# item = "moon"
 
 # print("The " + animal + " " + "jumped over the " + item)
-print("The {} jumped over the {}".format(animal, item))
+# print("The {} jumped over the {}".format(animal, item))
+# print("The {1} jumped over the {0}".format(animal, item)) #positional argument
+print("The {item} jumped over the {animal}".format(animal="cow", item="moon")) #keyword argument
 
