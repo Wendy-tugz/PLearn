394a4d8c8aa10389af7bdac47339b3ef448a5b14
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-02 11:59:52 +0300

    Learn @Wendy-tugz str.format

--- functions.py
+++ functions.py
@@ -88,3 +88,5 @@ print("Hello my name is {}".format(name))
 print("Hello my name is {:10} Nice to meet you".format(name))
 print("Hello my name is {:<10} Nice to meet you".format(name))
 print("Hello my name is {:>10} Nice to meet you".format(name))
+print("Hello my name is {:^10} Nice to meet you".format(name))
+
