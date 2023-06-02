1bc73c9efa9ef816ab1dc4ac33d4143a1b2bc3ce
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-02 14:42:10 +0300

    Learn @Wendy-tugz random module

--- functions.py
+++ functions.py
@@ -90,12 +90,31 @@
 # print("Hello my name is {:>10} Nice to meet you".format(name))
 # print("Hello my name is {:^10} Nice to meet you".format(name))
 
-number = 1000
+# number = 1000
+#
+# print("The number pi is {:.3f}".format(number))
+# print("The number is {:,}".format(number))
+# print("The number is {:b}".format(number))
+# print("The number is {:o}".format(number))
+# print("The number is {:x}".format(number))
+# print("The number is {:E}".format(number))
 
-print("The number pi is {:.3f}".format(number))
-print("The number is {:,}".format(number))
-print("The number is {:b}".format(number))
-print("The number is {:o}".format(number))
-print("The number is {:x}".format(number))
-print("The number is {:E}".format(number))
+# random module
+
+import random
+x = random.randint(1, 6)
+y = random.random()
+
+myList = ['rock', 'paper', 'scissors']
+z = random.choice(myList)
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "k", "A"]
+
+random.shuffle(cards)
+print(cards)
+
+# print(z)
+# print(x)
+# print(y)
+
 
