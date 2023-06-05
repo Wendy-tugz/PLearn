95f92531070a3cc40c413af044b76a77a67b9de9
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-05 16:16:47 +0300

    Learn @Wendy-tugz modules rock, paper, scissors games

--- modules.py
+++ modules.py
@@ -7,4 +7,15 @@
 # hello()
 # bye()
 
-help("modules")
+# help("modules")
+
+
+#rock, paper, scissors games
+import random
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+
+player = input("rock, paper, or scissors?: ")
+
+print(computer)
