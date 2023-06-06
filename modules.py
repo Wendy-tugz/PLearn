9c68a81ac176d0dddd36b96880625ac25d392b8a
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-06 11:03:56 +0300

    Learn @Wendy-tugz modules rock, paper, scissors games

--- modules.py
+++ modules.py
@@ -15,7 +15,40 @@ import random
 choices = ["rock", "paper", "scissors"]
 
 computer = random.choice(choices)
+player = None
 
-player = input("rock, paper, or scissors?: ")
+while player not in choices:
+    player = input("rock, paper, or scissors?: ").lower()
 
-print(computer)
+if player == computer:
+    print("player: ", player)
+    print("computer: ", computer)
+    print("Tie")
+elif player == "rock":
+    if computer == "paper":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You lose")
+    if computer == "scissors":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You win")
+elif player == "scissors":
+    if computer == "rock":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You lose")
+    if computer == "paper":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You win")
+elif player == "paper":
+    if computer == "scissors":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You lose")
+    if computer == "rock":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You win")
+
