7aff2eaabbb65ece44e3047e183b0941cb265d9b
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-06 11:33:20 +0300

    Learn @Wendy-tugz quiz game

---
+++ quizgame.py
@@ -0,0 +1,32 @@
+#--------------------------
+def new_game():
+    pass
+#--------------------------
+
+
+def check_answer():
+    pass
+#--------------------------
+
+
+def display_score():
+    pass
+#--------------------------
+
+
+def play_again():
+    pass
+#--------------------------
+
+
+questions = {
+    "Who created Python?: ": "A",
+    "What year was Python created?: ": "B",
+    "Python is tributed to which comedy group?: ": "C",
+    "Is the Earth round?: ": "A"    #dictionary
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. MArk Zuckerburg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+           ["A. Lonely", "B. Smosh", "C. SNL", "D. Monty"],
+           ["A. True", "B. False", "C. Sometimes", "D. What's Earrth?"]]    #2D list
