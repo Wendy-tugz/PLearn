0d29d872c19fc44107c3fb4b5d8bbe923282fecf
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-06 13:13:28 +0300

    Learn @Wendy-tugz quiz game

--- quizgame.py
+++ quizgame.py
@@ -1,6 +1,6 @@
 #--------------------------
 def new_game():
-    guesses =[]
+    guesses = []
     correct_guesses = 0
     question_num = 1
 
@@ -9,23 +9,57 @@ def new_game():
         print(key)
         for i in options[question_num - 1]:
             print(i)
+        guess = input("Enter (A, B, C or D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key), guess)
 
         question_num += 1
+
+    display_score(correct_guesses, guesses)
 #--------------------------
 
 
-def check_answer():
-    pass
+def check_answer(answer, guess):
+    if answer == guess:
+        print("Correct!")
+        return 1
+    else:
+        print("Wrong!")
+        return 0
 #--------------------------
 
 
-def display_score():
-    pass
+def display_score(correct_guesses, guesses):
+    print("--------------------------")
+    print("RESULTS")
+    print("--------------------------")
+    print("Answers: ", end=" ")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end=" ")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = (correct_guesses/len(questions) *100)
+    print("Your score is:" + str(score) + "%")
 #--------------------------
 
 
 def play_again():
-    pass
+
+    response = input("Do you want to play again? (yes or no): ")
+    response = response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
+
 #--------------------------
 
 
@@ -39,6 +73,11 @@ questions = {
 options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. MArk Zuckerburg"],
            ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
            ["A. Lonely", "B. Smosh", "C. SNL", "D. Monty"],
-           ["A. True", "B. False", "C. Sometimes", "D. What's Earrth?"]]    #2D list
+           ["A. True", "B. False", "C. Sometimes", "D. What's Earth?"]]    #2D list
 
 new_game()
+
+while play_again():
+    new_game()
+
+print("Byeee!!!")
