7eb8fbfb09db5a12cbb56f95d5e85c6a24661f55
Wendy-tugz <wtugahirwa@gmail.com>
2023-06-16 16:52:25 +0300

    Learn @Wendy-tugz inheritance

--- inheritance.py
+++ inheritance.py
@@ -10,12 +10,28 @@ class Animal:
 
 
 class Rabbit(Animal):
-    pass
+    def run(self):
+        print("This rabbit is running.")
 
 
 class Fish(Animal):
-    pass
+    def swim(self):
+        print("This fish is swimming.")
 
 
 class Hawk(Animal):
-    pass
+    def fly(self):
+        print("This hawk is flying.")
+
+
+rabbit = Rabbit()
+fish = Fish()
+hawk = Hawk()
+
+# print(rabbit.alive)
+# fish.eat()
+# hawk.sleep()
+
+rabbit.run()
+fish.swim()
+hawk.fly()
