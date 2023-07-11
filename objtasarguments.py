6521c7f92c28ab967aabdc2660468d473acf8928
joekingsleyMukundi <joekingsleymukundi@gmail.com>
2023-07-11 18:17:13 +0300

    Learn @Wendy-tugz duck typing

--- objtasarguments.py
+++ objtasarguments.py
@@ -3,19 +3,28 @@ class Car:
     color = None
 
 
-def change_color(car, color):
+class Motorcycle:
 
-    car.color = color
+    color = None
+
+
+def change_color(vehicle, color):
+
+    vehicle.color = color
 
 
 car_1 = Car()
 car_2 = Car()
 car_3 = Car()
 
+bike_1 = Motorcycle()
+
 change_color(car_1, "pink")
 change_color(car_2, "white")
 change_color(car_3, "blue")
+change_color(bike_1, "black")
 
 print(car_1.color)
 print(car_2.color)
 print(car_3.color)
+print(bike_1.color)
