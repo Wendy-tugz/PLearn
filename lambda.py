e8de78f7cbe79aea7741cffb574ae08cabfdc7e4
joekingsleyMukundi <joekingsleymukundi@gmail.com>
2023-07-12 14:45:17 +0300

    Learn @Wendy-tugz lambda

---
+++ lambda.py
@@ -0,0 +1,10 @@
+# lambda = func written in 1 line using lambda keyword
+#          accepts any number of arguments, but only one expression
+#          useful if needed for a short period of time
+
+# def double(x):
+#     return x * 2
+# print(double(5))
+
+double = lambda x: x * 2
+print(double(5))
