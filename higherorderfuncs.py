c26bb6c1cf5a326a0b935945813fa7bc64294f01
joekingsleyMukundi <joekingsleymukundi@gmail.com>
2023-07-12 14:38:31 +0300

    Learn @Wendy-tugz higher order functions

--- higherorderfuncs.py
+++ higherorderfuncs.py
@@ -1,15 +1,28 @@
 #higher order function = a function that either accepts a function as an argument OR returns a function
 
-def loud(text):
-    return text.upper()
+# def loud(text):
+#     return text.upper()
+#
+#
+# def quiet(text):
+#     return text.lower()
+#
+#
+# def hello(func):
+#     text = func("Hello")
+#     print(text)
+#
+#
+# hello(quiet)
+# hello(loud)
+
+#returns a function
+
+def divisor(x):
+    def dividend(y):
+        return y / x
+    return dividend
 
 
-def quiet(text):
-    return text.lower()
-
-
-def hello(func):
-    text = func("Hello")
-    print(text)
-
-hello(quiet)
+divide = divisor(2)
+print(divide(10))
