380ef59e170557c541f22fb13da73a3df2c09f43
joekingsleyMukundi <joekingsleymukundi@gmail.com>
2023-07-12 19:44:30 +0300

    Learn @Wendy-tugz lambda

--- lambda.py
+++ lambda.py
@@ -10,7 +10,10 @@
 double = lambda x: x * 2
 multiply = lambda x, y: x * y
 add = lambda x, y, z: x + y + z
+full_name = lambda fname, lname: fname + " " + lname
+age_check = lambda age: True
 # print(double(5))
 # print(multiply(5, 6))
-print(add(5, 6, 7))
+# print(add(5, 6, 7))
+# print(full_name("Wendy", "Tugz"))
 
