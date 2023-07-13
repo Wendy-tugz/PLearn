8a1aa145698322bc48a2da5e88fc2f49578578b4
joekingsleyMukundi <joekingsleymukundi@gmail.com>
2023-07-13 12:35:19 +0300

    Learn @Wendy-tugz sort menthod, function

---
+++ sort.py
@@ -0,0 +1,9 @@
+#sort() method = used with lists
+#sort() function = used with iterables
+
+students = ["Squid", "Sandy", "Patrick", "Spongebob", "Karbs"]
+
+students.sort()
+
+for i in students:
+    print(i)
