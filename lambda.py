4d7f03ab74add0f446b8f19a9a3f8975fbcb2052
joekingsleyMukundi <joekingsleymukundi@gmail.com>
2023-07-13 12:30:05 +0300

    Learn @Wendy-tugz lambda

--- lambda.py
+++ lambda.py
@@ -11,9 +11,10 @@ double = lambda x: x * 2
 multiply = lambda x, y: x * y
 add = lambda x, y, z: x + y + z
 full_name = lambda fname, lname: fname + " " + lname
-age_check = lambda age: True
+age_check = lambda age: True if age >= 18 else False
 # print(double(5))
 # print(multiply(5, 6))
 # print(add(5, 6, 7))
 # print(full_name("Wendy", "Tugz"))
+print(age_check(18))
 
