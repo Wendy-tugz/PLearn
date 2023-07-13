4125216b0045b17cc8475bbacd05b55ffbe65e60
Wendy-tugz <wtugahirwa@gmail.com>
2023-07-13 16:08:29 +0300

    Learn @Wendy-tugz sort menthod, function

--- sort.py
+++ sort.py
@@ -20,17 +20,18 @@
 
 
 #tuple
-students = [("Squid", "F", 60),
+students = (("Squid", "F", 60),
             ("Sandy", "A", 33),
             ("Patrick", "D", 36),
             ("Spongebob", "B", 20),
-            ("Karbs", "C", 78)]
+            ("Karbs", "C", 78))
 
 # grade = lambda grades: grades[1]
 # students.sort(key=grade, reverse=True)
 
 age = lambda ages: ages[2]
-students.sort(key=age)
+# students.sort(key=age)
+sorted_students = sorted(students, key=age)
 
-for i in students:
+for i in sorted_students:
     print(i)
